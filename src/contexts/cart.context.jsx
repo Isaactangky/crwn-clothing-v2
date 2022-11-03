8f24677ae461b37efb514d8a3016f035fc1d8a89
Isaactangky <75126969+Isaactangky@users.noreply.github.com>
2022-11-03 16:56:29 +0800

    add addToCart feature

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,42 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+const addCartItem = (cartItems, productToAdd) => {
+  // find if cartItems contains productToAdd
+  const itemFound = cartItems.find((item) => item.id === productToAdd.id);
+  // if found, increment quantity
+  if (itemFound)
+    return [
+      ...cartItems.filter((item) => item.id !== itemFound.id),
+      { ...itemFound, quantity: itemFound.quantity + 1 },
+    ];
 
+  // return new array of modified cartItems
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  totalQuantity: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+  const [totalQuantity, setTotalQuantity] = useState(0);
+  useEffect(() => {
+    const newQuantity = cartItems.reduce((sum, cur) => sum + cur.quantity, 0);
+    setTotalQuantity(newQuantity);
+  }, [cartItems]);
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    totalQuantity,
+  };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 // import SHOP_DATA from "../../shop-data.json";
 import { ProductsContext } from "../../contexts/products.context";
+
 import { useContext } from "react";
 import ProductCard from "../../components/product-card/product-card.component";
 import "./shop.styles.scss";
